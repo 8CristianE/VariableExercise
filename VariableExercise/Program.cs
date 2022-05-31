9bf675c8e12ce1f0b8f9b2f0c3f0be573a732294
cristianespinosa7621@gmail.com <email@example.com>
2022-05-31 12:16:06 -0700

    initial commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Cristian";
+
+            int myAge = 18;
+
+            char myLastNameInitial = 'E';
+
+            bool cSharp = true;
+
+            double myWeight = 185.5;
+
+            decimal myHeight = 68.4m;
+
+            Console.WriteLine($"My name is {myName} and my last name inital is {myLastNameInitial}.");
+            Console.WriteLine($"I am {myAge} years old and it is {cSharp} im learning C#.");
+            Console.WriteLine($"My weight is {myWeight} pounds and my height is {myHeight} inches.");
+
+        }
+    }
+}
+
